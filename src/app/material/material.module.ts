262ec13e40902d4be776191219cb74f659c6e602
Ilya kirilushkin <ilya.kirilushkin@zimad.com>
2019-10-08 11:53:46 +0300

    use custom theme for seat component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatToolbarModule, MatButtonModule],
-  exports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatRippleModule],
+  exports: [MatToolbarModule, MatButtonModule, MatRippleModule],
   declarations: [],
   providers: []
 })
